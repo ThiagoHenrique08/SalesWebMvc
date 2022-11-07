16acda1c66c5da10227f8403e890ef3a3b203859
Thiago Henrique <thiagohenrique.jobs@gmail.com>
2022-11-07 20:46:39 -0300

    Delete seller

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,7 +1,6 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using SalesWebMvc.Models;
 using SalesWebMvc.Data;
 
@@ -27,6 +26,18 @@ namespace SalesWebMvc.Services
             _context.SaveChanges();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+        }
 
     }
+
 }
