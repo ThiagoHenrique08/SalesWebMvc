b693f78251a78d79d88779cade0cb2089acb37f5
Thiago Henrique <thiagohenrique.jobs@gmail.com>
2022-11-07 08:05:25 -0300

    Criacao da tela de cadastro da view Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+
 
 namespace SalesWebMvc.Controllers
 {
@@ -22,6 +24,18 @@ namespace SalesWebMvc.Controllers
             return View(list);
         }
 
+        public IActionResult Create()
+        {
+            return View();
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
