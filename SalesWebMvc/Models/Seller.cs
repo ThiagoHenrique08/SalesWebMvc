a91641ccc41ec2e3cca20bb611c4a0f278fa0f27
Thiago Henrique <thiagohenrique.jobs@gmail.com>
2022-11-07 08:22:06 -0300

    Inserindo Foreign Key não nula

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -13,6 +13,7 @@ namespace SalesWebMvc.Models
         public double BaseSalary { get; set; }
 
         public Departament Departament { get; set; }
+        public int DepartamentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
        public Seller()
@@ -28,6 +29,7 @@ namespace SalesWebMvc.Models
             BirthDate = birthDate;
             BaseSalary = baseSalary;
             Departament = departament;
+          
         }
 
 
